5b25e69b37b9bfeb03e4928b884c61ccd76eccdc
Vedant Pandey <ved2003ant@gmail.com>
2026-01-27 11:13:27 +0530

    Bank Account Association Problem

---
+++ BridgeLabz_Practice/BankAssociationAccountHolder/BankAssociationAccountHolder/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace BankAssociationAccountHolder
+{
+    public class Account
+    {
+        public int accountNumber {  get; }
+        public double balance {get; set;}
+        public Bank Bank { get; }
+
+        public Account (int accountNumber, Bank bank)//intialization constructor
+        {
+            this.accountNumber = accountNumber;
+            Bank = bank;
+        }
+
+        public void Deposit (double amount)
+        {
+            balance += amount;
+        }
+    }
+
+    public class Customer
+    {
+        public string name { get; }
+        private List<Account> accounts;
+
+        public Customer(string name)
+        {
+            this.name = name;
+            accounts = new List<Account>();
+        }
+
+        public void AddAccount(Account account)
+        {
+            accounts.Add(account);
+        }
+
+        public void ViewBalance()
+        {
+            Console.WriteLine($"Balance for {name}");
+            foreach (var account in accounts)
+            {
+                Console.WriteLine(
+                    $"Bank: {account.Bank.BankName}," +
+                    $"Account No: {account.accountNumber}," +
+                    $"Balance: {account.balance}"
+                    );
+            }
+        }
+    }
+
+    public class Bank
+    {
+        public string BankName { get; }
+        private List<Account> accounts;
+        private int nextAccountNumber = 1001;
+
+        public Bank(string bankName)
+        {
+            this.BankName = bankName;
+            accounts = new List<Account>();
+        }
+
+        public Account NewAccount (Customer customer)
+        {
+            Account account = new Account(nextAccountNumber++, this);
+            accounts.Add(account);
+            customer.AddAccount(account);
+
+            return account;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank hdfc = new Bank("HDFC Bank");
+            Bank sbi = new Bank("SBI");
+
+            Customer A = new Customer("A");
+
+            Account acc1 = hdfc.NewAccount(A);
+            Account acc2 = sbi.NewAccount(A);
+
+            Customer B = new Customer("B");
+            Account acc3 = hdfc.NewAccount(B);
+            
+
+            acc1.Deposit(83924);
+            acc2.Deposit(1234);
+
+            A.ViewBalance();
+            B.ViewBalance();
+        }
+    }
+}
---
+++ LibraryBookAgregation/LibraryBookAgregation/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace LibraryBookAgregation
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public Book(string title, string author)
+        {
+            this.Title = title;
+            this.Author = author;
+        }
+
+        public void DisplayBookInfo()
+        {
+            Console.WriteLine($"Title: {Title}, Author: {Author}");
+        }
+    }
+
+    class Library
+    {
+        public string LibraryName { get; set; } //diff libraries have diff names
+        public List<Book> books { get; set; } //list of books in specific library
+
+        public Library(string libraryName)
+        {
+            this.LibraryName = libraryName;
+            this.books = new List<Book>();
+        }
+        public void AddBook(Book book)
+        {
+            books.Add(book); 
+        }
+        public void ShowLibraryBooks()
+        {
+            Console.WriteLine($" Books in {LibraryName}");
+            foreach (var book in books)
+            {
+                book.DisplayBookInfo();
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("abc", " Mr. A");
+            Book book2 = new Book("def", "Mr. B");
+
+            Library lib1 = new Library("xyz Library");
+            Library lib2 = new Library("fgh Library");
+
+            lib1.AddBook(book1);// each lib gets a copy of specific book object
+            lib1.AddBook(book2);
+
+            lib2.AddBook(book2);
+
+            lib1.ShowLibraryBooks();
+
+            Console.WriteLine();
+
+            lib2.ShowLibraryBooks();
+
+        }
+    }
+}
