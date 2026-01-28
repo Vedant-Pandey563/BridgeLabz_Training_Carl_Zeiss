125eb29c1b8f60c224535e74c0688b9b01a961ab
Vedant Pandey <ved2003ant@gmail.com>
2026-01-28 16:24:24 +0530

    Added HealthCare And Line Programs

---
+++ BridgeLabz_Practice/HealthCareRecord/HealthCareRecord/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace HealthCareRecord
+{
+    class MedicalRecord
+    {
+        public string PatientName;
+        public int Age;
+        public double PatientID;
+
+        public MedicalRecord(string patientName, int age, double patientID)
+        {
+            this.PatientName = patientName;
+            this.Age = age;
+            this.PatientID = patientID;
+        }
+
+        public virtual void DisplaYPatientInfo()
+        {
+            Console.WriteLine($"Patient Name: {PatientName}");
+            Console.WriteLine($"Patient Age: {Age}");
+            Console.WriteLine($"Patient ID: {PatientID}");
+            Console.WriteLine();
+        }
+    }
+
+    class Presciption : MedicalRecord
+    {
+        public string MedicineName;
+        public int DailyDose;
+
+        public Presciption (string patientName, int age, double patientID, string medicineName, int dailyDose)
+            : base(patientName, age, patientID)
+        {
+            this.MedicineName = medicineName;
+            this.DailyDose = dailyDose;
+        }
+
+        public override void DisplaYPatientInfo()
+        {
+            Console.WriteLine($"Medicine Name: {MedicineName}");
+            Console.WriteLine($"Daily Dose: {DailyDose}");
+            Console.WriteLine();
+        }
+    }
+
+    class LabReport : MedicalRecord
+    {
+        public string TestName;
+        public bool Result;
+
+        public LabReport(string patientName, int age,double patientID, string testname,bool result)
+            :base(patientName,age,patientID)
+        {
+            this.TestName = testname;
+            this.Result = result;
+        }
+
+        public override void DisplaYPatientInfo()
+        {
+            Console.WriteLine($"Test Name: {TestName}");
+            Console.WriteLine($"Result: {Result}");
+            Console.WriteLine();
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HealthCare Record");
+
+            MedicalRecord record1 = new MedicalRecord("A", 45, 123456);
+            record1.DisplaYPatientInfo();
+
+            Presciption prescription1 = new Presciption("A",45,123456,"Crocin",3);
+            prescription1.DisplaYPatientInfo();
+
+            LabReport labReport1 = new LabReport("A",45,123456,"Diabetes", true);
+            labReport1.DisplaYPatientInfo();
+        }
+    }
+}
---
+++ BridgeLabz_Practice/Line/Line/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Line
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point (int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+
+    class Line
+    {
+        public Point Start;
+        public Point End;
+
+        public Line(Point start,Point end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        public double CalcLength()
+        {
+            double xLength = End.X- Start.X;
+            double yLength = End.Y - Start.Y;
+            double length = Math.Sqrt(Math.Pow(xLength,2) + Math.Pow(yLength,2));
+            return length;
+        }
+
+        public void LengthCompare(Line Line2)
+        {
+            double length1 = this.CalcLength();
+            double length2 = Line2.CalcLength();
+
+            if(length1 > length2)
+            {
+                Console.WriteLine("Line 1 is longer than Line 2");
+            }
+            else if(length1 < length2)
+            {
+                Console.WriteLine("Line 2 is longer than Line 1");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are of equal length");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Line Line1 = new Line(
+                new Point(1, 2),
+                new Point(3, 6));
+
+            Line Line2 = new Line(
+                new Point(5, 6),
+                new Point(7, 9));
+
+            Console.WriteLine(Line1.CalcLength());
+            Console.WriteLine(Line2.CalcLength());
+
+            Line1.LengthCompare(Line2);
+        }
+    }
+}
