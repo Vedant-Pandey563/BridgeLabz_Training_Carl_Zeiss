c73a46e8751eda2eb133614b5265c924971bf6dc
Vedant Pandey <ved2003ant@gmail.com>
2026-01-31 12:47:24 +0530

    Added Codes for Singly Doubly Linked List

---
+++ DSAinC#/DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace DoublyLinkedList
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node prev;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+            prev = null;
+        }
+    }
+    class DoublyLinkedList
+    {
+        private Node head;
+        public void PrintList()
+        {
+            Node temp = head;
+
+            while (temp != null)
+            {
+                Console.Write(temp.data + " <-> ");
+                temp = temp.next;
+            }
+            Console.WriteLine("Null");
+            Console.WriteLine();
+        }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node temp = head;
+
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = newNode;
+            newNode.prev = temp;
+
+            return;
+        }
+
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+
+            if(head != null)
+            {
+                head.prev = newNode;
+            }
+
+            newNode.next = head;
+            newNode.prev = null;
+
+            head = newNode;
+        }
+
+        public void AddIndex(int index,int data)
+        {
+            Node newNode = new Node(data);
+
+            if (index == 0)
+            {
+                AddFirst(data);
+                return;
+            }
+            int cnt = 0;
+
+            Node temp = head;
+
+            while(cnt != index && temp != null)
+            {
+                cnt++;
+                temp = temp.next;
+            }
+
+            Node prevNode = temp.prev;
+
+            newNode.prev = prevNode;
+            prevNode.next = newNode;
+            newNode.next = temp;
+            temp.prev = newNode;
+
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Doubly Linked List");
+            DoublyLinkedList l1 = new DoublyLinkedList();
+            l1.Add(1);
+            l1.PrintList();
+            
+            l1.Add(2);
+            l1.Add(3);
+            l1.PrintList();
+
+            l1.AddFirst(6);
+            l1.PrintList();
+
+            l1.AddIndex(3, 8);
+            l1.PrintList();
+
+            l1.AddIndex(0, 100);
+            l1.PrintList();
+        }
+    }
+}
---
+++ DSAinC#/LinkedList/LinkedList/LeetCodeRotateLL.cs
@@ -0,0 +1,50 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode RotateRight(ListNode head, int k)
+    {
+        if (head == null || head.next == null || k == 0)
+        {
+            return head;
+        }
+        int length = 0;
+        ListNode temp = head;
+        while (temp != null)
+        {
+            length++;
+            temp = temp.next;
+        }
+
+        k = k % length;
+
+        int cnt = 0;
+        while (cnt < k)
+        {
+            ListNode curr = head;
+
+            while (curr.next.next != null)
+            {
+                curr = curr.next;
+            }
+
+            ListNode last = curr.next;
+            curr.next = null;
+
+            last.next = head;
+
+            head = last;
+            cnt++;
+        }
+        return head;
+    }
+}
