0c19c9e2aeb1a38d3b2f1737d7286942be831cc2
Vedant Pandey <ved2003ant@gmail.com>
2026-01-31 15:41:54 +0530

    Added Codes for Linked List , Employee Linked List

---
+++ DSAinC#/EmployeeLinkedList/EmployeeLinkedList/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace EmployeeLinkedList
+{
+    class Employee
+    {
+        public int EmpId;
+        public string EmpName;
+        public string dept;
+        public double salary;
+
+        public Employee (int EmpId, string EmpName, string dept, double salary)
+        {
+            this.EmpId = EmpId;
+            this.EmpName = EmpName;
+            this.dept = dept;
+            this.salary = salary;
+        }
+    }
+
+    class EmpNode
+    {
+        public Employee data;
+        public EmpNode next;
+
+        public EmpNode(Employee data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    class EmployeeLinkedList
+    {
+        private EmpNode head;
+
+        public void Add(Employee emp)
+        {
+            EmpNode newNode = new EmpNode(emp);
+
+            if(head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            EmpNode temp = head;
+
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = newNode;
+        }
+
+        public void Print()
+        {
+            EmpNode temp = head;
+
+            while(temp != null)
+            {
+                Console.Write(temp.data.EmpName + "->");
+
+                temp = temp.next;
+            }
+
+            Console.WriteLine("Null");
+            Console.WriteLine();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Linked List");
+
+            Employee emp1 = new Employee(2,"A","CS",2345);
+            Employee emp2 = new Employee(3, "cdji", "scs", 23);
+
+            EmployeeLinkedList list1 = new EmployeeLinkedList();
+
+            list1.Add(emp1);
+            list1.Print();
+
+            list1.Add(emp2);
+            list1.Print();
+        }
+    }
+}
---
+++ DSAinC#/LinkedList/LinkedList/LeetCodeReorderLL.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+
+    public void ReorderList(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null)//fast is 2x of slow , when fast is at  end slow will be at mid
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = slow.next; //second half of list starts from mid
+        slow.next = null;
+
+        ListNode prev = null;
+
+        //reversing the second half
+        while (second != null)
+        {
+            ListNode temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+
+        ListNode first = head;
+        second = prev;
+
+        while (second != null)
+        {
+            ListNode x = first.next;
+            ListNode y = second.next;
+
+            first.next = second;
+            second.next = x;
+
+            first = x;
+            second = y;
+        }
+
+    }
+}
