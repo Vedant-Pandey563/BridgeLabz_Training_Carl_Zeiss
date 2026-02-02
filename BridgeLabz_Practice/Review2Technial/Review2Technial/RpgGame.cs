a4984d4070c5cd6ee8b40fd60ad6e941d6c67ac7
Vedant Pandey <ved2003ant@gmail.com>
2026-02-02 16:35:11 +0530

    Review 2 technial codes added and fixed repo folders

---
+++ BridgeLabz_Practice/Review2Technial/Review2Technial/RpgGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Warrior
+{
+    public virtual double Attack()
+    {
+        Console.WriteLine("Warrior attacks");
+        return 50;
+    }
+}
+
+class Paladin : Warrior
+{
+    public override double Attack()
+    {
+        double baseAttack = base.Attack();
+        double boostedAttack = baseAttack * 1.2;
+        Console.WriteLine("Paladin attacks");
+        return boostedAttack;
+    }
+
+    public void Heal()
+    {
+        Console.WriteLine("Paladin heal for 30");
+    }
+}
+
+public class UniversalRemote
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("RPG Game\n");
+
+        Warrior w = new Warrior();
+        Console.WriteLine("Warrior Attack : " + w.Attack());
+
+        Console.WriteLine("Level Up to paladin");
+
+        Paladin p = new Paladin();
+        Console.WriteLine("Paladin Attack : " + p.Attack());
+        p.Heal();
+    }
+}
---
+++ BridgeLabz_Practice/Review2Technial/Review2Technial/UniversalRemote.cs
@@ -0,0 +1,64 @@
+﻿// Universal Rempte
+using System;
+using System.Collections.Generic;
+
+class Warrior
+{
+    public double attack { get; set; }
+    public virtual void Attack(double attack)
+    {
+        attack = +5;
+    }
+}
+
+class Paladin : Warrior
+{
+    public double heal { get; set}
+
+    public override void Attack(attack)
+    {
+        attack = 1.2 * Warrior().Attack();
+    }
+
+    public void Heal()
+    {
+        heal = +30;
+    }
+}
+
+
+class Remote
+{
+    private List<Device> device = new List<Device>();
+
+    public void AddDevice(Device d)
+    {
+        device.Add(d);
+    }
+
+    public void SwitchOn()
+    {
+        foreach (Device d in device)
+        {
+            d.Turn_on();
+        }
+    }
+
+}
+
+public class UniversalRemote
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Universal Remote");
+
+        Remote r = new Remote();
+
+        r.AddDevice(new TV());
+        r.AddDevice(new Light());
+        r.AddDevice(new AC());
+
+        r.SwitchOn();
+
+    }
+}
