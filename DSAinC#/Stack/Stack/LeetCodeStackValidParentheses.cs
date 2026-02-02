df36b4ce6d824d1b3bde80977cebfa53722d4925
Vedant Pandey <ved2003ant@gmail.com>
2026-02-02 17:46:24 +0530

    Stack Leetcode Problems

---
+++ DSAinC#/Stack/Stack/LeetCodeStackValidParentheses.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        Dictionary<char, char> bracketCheck = new Dictionary<char, char>()
+        {
+            {')','('},
+            {']','['},
+            {'}','{'}
+        };
+
+        //traverse i/p str
+        foreach (char c in s)
+        {
+            if (bracketCheck.ContainsKey(c))
+            {
+                // stack not empty + bracket match
+                if (stack.Count > 0 && stack.Peek() == bracketCheck[c])
+                {
+                    stack.Pop();// remove current bracket from stack
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else// it is a opening bracket stored in key of dict  
+            {
+                stack.Push(c);
+            }
+        }
+
+        return stack.Count == 0;//return true only if stack is empty
+    }
+}
