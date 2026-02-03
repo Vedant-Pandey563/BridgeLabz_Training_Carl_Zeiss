f51bcbdcf89671a9cd46ccd0f86edfda6a6bcf08
Vedant Pandey <ved2003ant@gmail.com>
2026-02-03 17:41:55 +0530

    Added Queue Problems

---
+++ DSAinC#/Queue/Queue/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Queue");
+
+            Queue<int> q = new Queue<int>();
+
+            q.Enqueue(1);
+            q.Enqueue(2);
+            Console.WriteLine( q.Peek());
+            Console.WriteLine( q.Dequeue());
+
+            q.Enqueue(3);
+            q.Enqueue(4);
+
+            Console.WriteLine(q.Contains(4));
+            Console.WriteLine(q.Contains(9));
+
+            Console.WriteLine(q.Dequeue());
+            Console.WriteLine(q.Count());
+
+            q.Clear();
+
+            Console.WriteLine(q.Dequeue());
+            Console.WriteLine( q.Contains(3));
+
+            Console.WriteLine(q.Count());
+
+        }
+    }
+}
---
+++ DSAinC#/Queue/Queue/TicketQueue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    class Ticket
+    {
+        Queue<string> q = new Queue<string>();
+
+        public void AddCustomer(string name)
+        {
+            q.Enqueue(name);
+            Console.WriteLine($"Added {name} to Queue");
+        }
+
+        public void ServeCustomer()
+        {
+            if (q.Count == 0)
+            {
+                Console.WriteLine("Queue is Empty");
+                Console.WriteLine();
+                return;
+            }
+
+            string x = q.Dequeue();
+            Console.WriteLine($"{x} has been served");
+            Console.WriteLine();
+        }
+
+        public void ShowQueue()
+        {
+            if (q.Count == 0)
+            {
+                Console.WriteLine("Queue is Empty");
+                Console.WriteLine();
+                return;
+            }
+
+            Console.WriteLine("People in queue");
+            
+            foreach(string s in q)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+        }
+
+    }
+    internal class TicketQueue
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ticket System");
+            Console.WriteLine();
+
+            Ticket t = new Ticket();
+
+            t.AddCustomer("A");
+            t.AddCustomer("B");
+            t.AddCustomer("C");
+
+            t.ShowQueue();
+            t.ServeCustomer();
+
+            t.ServeCustomer();
+
+            t.AddCustomer("D");
+
+            t.ShowQueue();
+            t.ServeCustomer();
+            t.ServeCustomer();
+            t.ShowQueue();
+        }
+    }
+}
