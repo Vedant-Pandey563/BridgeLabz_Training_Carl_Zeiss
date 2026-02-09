1a4d6dd297705590e571df7d95fc3001510e9148
Vedant Pandey <ved2003ant@gmail.com>
2026-02-09 17:23:14 +0530

    C# Delegate and Events

---
+++ CollectionsPractice/GenericsPractice/GenericsPractice/DelegatesDemo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsPractice
+{
+    internal class DelegatesDemo
+    {
+        delegate void MyDelegate(string msg);
+
+        static void ShowMessage(string msg)
+        {
+            Console.WriteLine("Msg : "+msg);
+        }
+
+        static void A(string s) => Console.WriteLine("A: "+s);
+        static void B(string s) => Console.WriteLine("B : " + s);
+
+        delegate int MathOp(int x, int y);
+
+        static int Add(int a, int b) => a + b;
+        static int Subtract(int a, int b) => a - b;
+
+        static void Process(Action task)
+        {
+            Console.WriteLine("Starting Task");
+            task();
+            Console.WriteLine("Task Finished");
+        }
+
+        static void Main()
+        {
+            MyDelegate d = ShowMessage;
+            d("Hello Delegate Method");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            Console.WriteLine("Multicast Delegates");
+            MyDelegate Md = A;
+            Md += B;
+            Md("Hi");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            Console.WriteLine("Delegate + Return Values");
+            MathOp op = Add;
+            Console.WriteLine(op(5,3));
+
+            op = Subtract;
+            Console.WriteLine(op(5,3));
+            Console.ReadLine();
+
+            Console.WriteLine("Built in Delegates");
+            Action<string> print = s => Console.WriteLine(s);
+            print("Hello Action");
+            Console.WriteLine();
+            Func<int, int, int> add = (a, b) => a + b;
+            Console.WriteLine(add(10, 5));
+            Console.ReadLine();
+
+            Console.WriteLine("Delegate as Parameter");
+            Process(() => Console.WriteLine("Doing Work"));
+
+        }
+
+    }
+}
---
+++ CollectionsPractice/GenericsPractice/GenericsPractice/EventDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsPractice
+{
+    class Button //publisher
+    {
+        public event Action Clicked; //event based on built in delegate action
+
+        public void Press()
+        {
+            Console.WriteLine("Button was Pressed");
+
+            Clicked?.Invoke();//raise event safely only if subscribers present
+        }
+    }
+
+    class ButtonEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+    }
+
+    internal class EventDemo
+    {
+       static void OnButtonClicked()
+       {
+         Console.WriteLine("Event Recieved by Subscriber");
+       }
+       static void Main()
+       {
+          Button b = new Button();
+
+          b.Clicked += OnButtonClicked;
+
+          b.Press();
+       }
+
+       }
+    }
