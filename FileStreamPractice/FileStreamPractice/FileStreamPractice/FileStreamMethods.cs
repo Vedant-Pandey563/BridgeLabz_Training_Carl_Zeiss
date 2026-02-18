eae040354e20efdca59be38a1d401de1096618b1
Vedant Pandey <ved2003ant@gmail.com>
2026-02-18 10:55:01 +0530

    Excpetions and File Handling Problems

---
+++ FileStreamPractice/FileStreamPractice/FileStreamPractice/FileStreamMethods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileStreamPractice
+{
+    internal class FileStreamMethods
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File Stream Methods");
+
+            FileStream fs = new FileStream("data.txt", FileMode.OpenOrCreate);
+
+            int data = fs.ReadByte();
+            Console.WriteLine("Reading 1 byte: "+data);
+
+            fs.WriteByte(65);
+
+
+
+            int Bytedata;
+            fs.Position = 0;
+
+            byte[] buffer = new byte[100];
+            fs.Read(buffer, 0, buffer.Length);
+
+            byte[] buffer2 = {65,66,67};
+            
+            fs.Write(buffer2,0,buffer2.Length);
+
+            Console.WriteLine(buffer);
+
+            fs.Position = 0;
+            while ((Bytedata = fs.ReadByte()) != -1)
+            {
+                Console.Write((char)Bytedata);
+            }
+
+            fs.Close();
+
+
+        }
+
+    }
+
+}
---
+++ FileStreamPractice/FileStreamPractice/FileStreamPractice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File Stream Practice");
+
+            FileStream fs = new FileStream("data.txt", FileMode.OpenOrCreate);
+
+            Console.WriteLine(Directory.GetCurrentDirectory());
+
+
+            int ByteData;
+
+            while((ByteData = fs.ReadByte())!=-1)
+            {
+                Console.Write((char)ByteData);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(fs.CanRead);
+            Console.WriteLine(fs.CanSeek);
+            Console.WriteLine(fs.CanWrite);
+            Console.WriteLine(fs.Position);
+            Console.WriteLine(fs.Length);
+
+
+            fs.Close();
+        }
+    }
+}
