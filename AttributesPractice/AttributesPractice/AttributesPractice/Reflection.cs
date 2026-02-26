6a2e0a6ba69ce862d0862e8dd3193a0550d4300f
Vedant Pandey <ved2003ant@gmail.com>
2026-02-26 17:23:47 +0530

    Attributes Practice Implementation

---
+++ AttributesPractice/AttributesPractice/AttributesPractice/Reflection.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AttributesPractice
+{
+    class Employee
+    {
+        public int Id;
+        public string Name {  get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine("Hello There");
+        }
+
+        public Employee()
+        {
+            Id = 0;
+            Name = "a";
+        }
+    }
+    internal class Reflection
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(Employee);
+
+            Console.WriteLine("Class Name :" +t.Name);
+            Console.WriteLine("NameSpace :" +t.Namespace);
+
+
+            Console.WriteLine("Class method info");
+            MethodInfo[] methods = t.GetMethods();
+
+            foreach (MethodInfo m in methods)
+            {
+                Console.WriteLine();
+                Console.Write("Method Name " +m.Name);
+                //Console.Write(" , Method return type " + m.ReturnType);
+                //Console.Write(" , Method return parameter " + m.ReturnParameter);
+                //Console.Write(" , Method Metadata token " + m.MetadataToken);
+                //Console.WriteLine(" , Method Declare type " + m.DeclaringType);
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Getting property info");
+            PropertyInfo[] prop = t.GetProperties();
+            foreach(PropertyInfo p in prop)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Property Name " +p.Name);
+                Console.WriteLine("Property Module " +p.Module);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Getting field info");
+            FieldInfo[] fields = t.GetFields();
+            foreach(FieldInfo f in fields)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Field Name " +f.Name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Getting Constructor info");
+            ConstructorInfo[] constructors = t.GetConstructors();
+            foreach(ConstructorInfo c in constructors)
+            {
+                Console.WriteLine("Constructor name " +c.Name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Object and Method using activator in relfection");
+            object obj = Activator.CreateInstance(t);
+            MethodInfo m2 = t.GetMethod("Show");
+            m2.Invoke(obj, null);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Property access using Reflection");
+            PropertyInfo p2 = t.GetProperty("Name");
+            p2.SetValue(obj, "PigeonDoctor");
+            Console.WriteLine(p2.GetValue(obj));
+            Console.WriteLine();
+
+            Console.WriteLine("Attribute using Reflections");
+            object[] attrs = t.GetCustomAttributes(false);
+            foreach(object attr in attrs)
+            {
+                Console.WriteLine(attr.GetType().Name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Accesing Assembly Info in Reflections");
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Type[] types = assembly.GetTypes();
+            foreach(Type ty in types)
+            {
+                Console.WriteLine("Assembly type name : " +ty.Name);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AttributesPractice/AttributesPractice/AttributesPractice/Validation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace AttributesPractice
+{
+    class User
+    {
+        [Required(ErrorMessage = "Name Required")]
+        [StringLength(20,ErrorMessage ="Name cannot be more than 20 characters")]
+        public string Name{ get; set; }
+
+        [Range(18, 60,ErrorMessage ="Age must be in between in 18 and 60")]
+        public int Age { get; set; }
+
+        [EmailAddress(ErrorMessage = "Enter valid email address")]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Passowrds do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+    internal class Validation
+    {
+        static void Main()
+        {
+            Console.WriteLine("Using Validation Attributes");
+
+            User u = new User();
+
+            Console.WriteLine("Enter Name : ");
+            u.Name = Console.ReadLine();
+            Console.WriteLine("Enter Age : ");
+            u.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Mail Id : ");
+            u.EmailAddress = Console.ReadLine();
+            Console.WriteLine("Enter Password : ");
+            u.Password = Console.ReadLine();
+            Console.WriteLine("Enter Confrim Password : ");
+            u.ConfirmPassword = Console.ReadLine();
+
+            //validation engine 
+
+            var context = new ValidationContext(u); // tels  system to validate curr obj
+
+            var results = new List<ValidationResult>(); //collects validation error results 
+
+            bool isValid = Validator.TryValidateObject(
+                u, // instance obj
+                context, // Validationcontext 
+                results, //Validationresults list
+                true);// validateAllProperties
+
+            if(isValid)
+            {
+                Console.WriteLine("Validation Successful");
+            }
+            else
+            {
+                Console.WriteLine("Validation Failed");
+                foreach(var error in results)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+        }
+    }
+}
